1ac9beadb9051d4ca578b1beb68ad0da7c1a87e1
Rémy-Christophe Schermesser <git@schermesser.com>
2011-07-11 16:30:04 +0200

    Add prop for problem 8

--- src/test/scala/test/P08Test.scala
+++ src/test/scala/test/P08Test.scala
@@ -7,6 +7,7 @@ import org.scalatest.prop.Checkers
 import org.scalacheck.Prop
 import org.scalacheck.Prop._
 import org.scalacheck.Arg
+import org.scalacheck.Gen
 import org.scalatest.prop.Checkers._
 import org.scalacheck.Arbitrary.arbitrary
 
@@ -14,7 +15,16 @@ import org.scalacheck.Arbitrary.arbitrary
 class P08Test extends FlatSpec with ShouldMatchers with Checkers {
   
   it should "validate the property" in {    
-    
+    val genIntList = Gen.containerOf[List, Int](Gen.frequency((1, 1), (2, 2), (3, 3)))
+    check( Prop.forAll(genIntList) { list: List[Int] => 
+      val a = P08.compress(list)
+      (for {
+        i <- 0 to (a.length-2)
+        n = a(i)
+        n_1 = a(i + 1)
+        if n == n_1
+      } yield n).isEmpty
+    })
   }
   
   it should "do nothing on a simple list" in {
