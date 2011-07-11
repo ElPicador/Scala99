e1822423d58a3e3cb9c858d9bfd769e276639c60
Rémy-Christophe Schermesser <git@schermesser.com>
2011-07-11 14:52:30 +0200

    Add property check for test 3

--- src/test/scala/test/P03Test.scala
+++ src/test/scala/test/P03Test.scala
@@ -3,7 +3,19 @@ package test
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
 
+import org.scalacheck.Prop
+import org.scalacheck.Prop._
+import org.scalacheck.Arg
+import org.scalatest.prop.Checkers._
+import org.scalacheck.Arbitrary.arbitrary
+
 class P03Test extends FlatSpec with ShouldMatchers {
+  it should "validate the property" in {    
+    check( Prop.forAll { (list: List[Int], n: Int) => 
+      (n > 0 && list.length > n) ==> 
+        (list(n) == P03.nth(n, list))
+    }, maxDiscarded(1000) )
+  }
   
   it should "get the n-th element" in {
     val list = List(1, 2, 3, 4, 5)
